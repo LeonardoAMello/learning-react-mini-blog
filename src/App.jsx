ef6820049b90744464824758f40e315caae0cee0
Leonardo Mello <leonardodeabreumello@gmail.com>
2024-03-06 20:40:22 -0300

    Current user saved on context and dinamic navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,44 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+// Context
+import { AuthProvider } from "./context/AuthContext";
+
+// Firebase Authentication
+import { onAuthStateChanged } from "firebase/auth";
+
+// Hooks
+import { useState, useEffect } from "react";
+import { useAuthentication } from "./hooks/useAuthentication";
+
 // CSS
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>;
+  }
+
   return (
     <div className="wrapper">
-      <Navbar />
-      <div className="container">
-        <Outlet />
-      </div>
-      <Footer />
+      <AuthProvider value={{ user }}>
+        <Navbar />
+        <div className="container">
+          <Outlet />
+        </div>
+        <Footer />
+      </AuthProvider>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,39 @@
 // Routing
 import { Link } from "react-router-dom";
 
+// Hooks
+import { useAuthentication } from "../hooks/useAuthentication";
+
+// Context
+import { useAuthValue } from "../context/AuthContext";
+
 // CSS
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+  const { user } = useAuthValue();
+
   return (
     <nav>
       <div className={styles.brand}>
         <span>Mini blog</span>
       </div>
       <div className={styles.links_list}>
-        <Link to="/">Início</Link>
-        <Link to="/new-post">Novo Post</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/about">Sobre</Link>
-        <Link to="/">Sair</Link>
+        {user ? (
+          <>
+            <Link to="/">Início</Link>
+            <Link to="/new-post">Novo Post</Link>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/about">Sobre</Link>
+            <Link to="/">Sair</Link>
+          </>
+        ) : (
+          <>
+            <Link to="/">Início</Link>
+            <Link to="/login">Entrar</Link>
+            <Link to="/register">Cadastre-se</Link>
+          </>
+        )}
       </div>
     </nav>
   );
