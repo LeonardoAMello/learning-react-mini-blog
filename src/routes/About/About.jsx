87e4bb4a7c4680b6f7a720c19f48a23d5bab3867
Leonardo Mello <leonardodeabreumello@gmail.com>
2024-03-06 20:40:39 -0300

    About page made

--- src/routes/About/About.jsx
+++ src/routes/About/About.jsx
@@ -1,10 +1,22 @@
+// Routing
+import { Link } from "react-router-dom";
+
 // CSS
 import styles from "./About.module.css";
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about_container}>
+      <div className={styles.about_box}>
+        <h2>
+          Sobre o Mini <span>Blog</span>
+        </h2>
+        <p>
+          Este projeto consiste em um blog feito com React no front-end e
+          Firebase no back-end.
+        </p>
+        <Link to="/new-post">Criar post</Link>
+      </div>
     </div>
   );
 };
