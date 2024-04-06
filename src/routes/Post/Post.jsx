acc837273a9b9886f4a6f40a4e0950e12b427191
Leonardo Mello <leonardodeabreumello@gmail.com>
2024-04-06 11:50:37 -0300

    Post page

--- src/routes/Post/Post.jsx
+++ src/routes/Post/Post.jsx
@@ -1,17 +1,41 @@
 // Routing
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+
+// Hooks
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 // CSS
 import styles from "./Post.module.css";
 
-const PostItem = ({ post }) => {
+const Post = () => {
   const { id } = useParams();
+  const { document: post, loading } = useFetchDocument("posts", id);
 
   return (
-    <div>
-      <h1>Post {id}</h1>
+    <div className={styles.post_container}>
+      {loading && <p>Carregando...</p>}
+      {post && (
+        <>
+          <div className={styles.post_main_content}>
+            <img src={post.image} alt={post.title} />
+            <div className={styles.post_description}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+
+          <h3>Este post trata sobre:</h3>
+          <div className={styles.tags}>
+            {post.tagsArray.map((tag) => (
+              <p key={tag}>
+                <span>#</span> {tag}
+              </p>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
 
-export default PostItem;
+export default Post;
